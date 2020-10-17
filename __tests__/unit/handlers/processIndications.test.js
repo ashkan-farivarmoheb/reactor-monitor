a3d6646ea76c051340f1189a0253a1a07a22f2e1
Ashkan <ashkan.farivar@snapp-box.com>
2020-10-18 01:56:17 +1100

    removed unsed test codes

--- __tests__/unit/handlers/processIndications.test.js
+++ __tests__/unit/handlers/processIndications.test.js
@@ -5,13 +5,7 @@ describe('Test for processIndications', function () {
     // This test invokes helloFromLambdaHandler() and compare the result 
     it('Verifies successful response', async () => {
         // Invoke helloFromLambdaHandler()
-        const result = await lambda.handler();
-        /* 
-            The expected result should match the return from your Lambda function.
-            e.g. 
-            if you change from `const message = 'Hello from Lambda!';` to `const message = 'Hello World!';` in processIndications.js
-            you should change the following line to `const expectedResult = 'Hello World!';`
-        */
+        // const result = await lambda.handler();
         // const expectedResult = 'Hello from Lambda!';
         // // Compare the result with the expected result
         // expect(result).toEqual(expectedResult);
